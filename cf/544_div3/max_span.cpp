5b24ac0a80ec78aff3d0f8446fd1098b18e56f59
fishy15 <aaryan.prakash3.14@gmail.com>
2019-03-08 10:12:44 -0800

    div3 f1 done

--- cf/544_div3/max_span.cpp
+++ cf/544_div3/max_span.cpp
@@ -27,3 +27,61 @@
 // :pray: :wu:
 // :pray: :vmaddur:
 // :pray: :dorijanko:
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct node {
+    vector<int> adj;
+    bool visited;
+};
+
+int n, e;
+vector<node> graph;
+
+int main() {
+    cin >> n >> e;
+    graph = vector<node>(n);
+
+    for (int i = 0; i < e; i++) {
+        int a, b; cin >> a >> b;
+        graph[a - 1].adj.push_back(b - 1);
+        graph[b - 1].adj.push_back(a - 1);
+    }
+
+    int maxDeg = 0;
+    int maxLoc = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (graph[i].adj.size() > maxDeg) {
+            maxDeg = graph[i].adj.size();
+            maxLoc = i;
+        }
+    }
+
+    //cerr << "\n\n";
+
+    graph[maxLoc].visited = true;
+    queue<pair<int, int>> q;
+    for (int i : graph[maxLoc].adj) {
+        q.push({i, maxLoc});
+    }
+
+    while (!q.empty()) {
+        pair<int, int> top = q.front();
+        q.pop();
+        if (graph[top.first].visited) continue;
+
+        cout << top.second + 1 << ' ' << top.first + 1<< '\n';
+        graph[top.first].visited = true;
+
+        for (int i : graph[top.first].adj) {
+            if (!graph[i].visited) q.push({i, top.first});
+        }
+    }
+
+    return 0;
+}
