135432056c304c683fbfc1c3062df96b19bdb927
fishy15 <aaryan.prakash3.14@gmail.com>
2019-03-09 23:35:37 -0800

    make template into real template

--- cf/545_div2/sushi.cpp
+++ cf/545_div2/sushi.cpp
@@ -29,17 +29,38 @@
 // :pray: :dorijanko:
 
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <utility>
-#include <map>
-#include <queue>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    cin.tie(0); ios::sync_with_stdio(0);
+int n;
+vector<int> nums;
 
-    return 0;
+int main() {
+    cin >> n;
+    int len = 0;
+    int prev = 0;                                                                                                    
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        if (x != prev) {
+            //cerr << len << endl;
+            if (len != 0) nums.push_back(len);
+            prev = x;
+            len = 1;
+        } else {
+            len++;
+        }
+    }
+
+    nums.push_back(len);
+    int ans = 0;
+
+    for (int i = 0; i < nums.size() - 1; i++) {
+        int val = min(nums[i], nums[i + 1]) * 2;
+        ans = val > ans ? val : ans;
+    }
+
+    cout << ans << '\n';
 }
+
