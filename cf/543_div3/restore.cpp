b0f4751ad68047a59af8e190d9de734dfa05c340
fishy15 <aaryan.prakash3.14@gmail.com>
2019-04-17 23:27:26 -0700

    restore

---
+++ cf/543_div3/restore.cpp
@@ -0,0 +1,71 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+using namespace std;
+
+int nums[4];
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    for (int i = 0; i < 4; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + 4);
+
+    for (int i = 0; i < 3; i++) {
+        cout << nums[3] - nums[i] << ' ';
+    }
+
+    return 0;
+}
