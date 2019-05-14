9ed66639191176e4a7bb47f27b3556b9e431b66f
fishy15 <aaryan.prakash3.14@gmail.com>
2019-05-14 13:38:16 -0700

    pile of stones

---
+++ 559_div2/pile_stones.cpp
@@ -0,0 +1,81 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+#define MAXN 100000
+
+using namespace std;
+
+int n;
+string s;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+    cin >> n >> s;
+    
+    int cur = 0;
+    int min = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '+') {
+            cur++;
+        } else {
+            cur--;
+            if (cur < min) min = cur;
+        }
+    }
+    
+    cout << cur - min << '\n';
+}
