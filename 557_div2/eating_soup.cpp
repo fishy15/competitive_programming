61dd8bd154d9fbfe438695cc219dff91dc4c1dbc
fishy15 <aaryan.prakash3.14@gmail.com>
2019-05-14 13:35:35 -0700

    eating soup

---
+++ 557_div2/eating_soup.cpp
@@ -0,0 +1,90 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n >> m;
+
+    if (n == 0) {
+        cout << 0 << '\n';
+        return 0;
+    }
+
+    if (m == 0) {
+        cout << 1 << '\n';
+        return 0;
+    }
+    
+    if (m == 1 && n > 1) {
+        cout << 1 << '\n';
+        return 0;
+    }
+
+    if (n / 2 >= m) {
+        cout << m << '\n';
+    } else {
+        m = n - m;
+        cout << m << '\n';
+    }
+
+    return 0;
+}
