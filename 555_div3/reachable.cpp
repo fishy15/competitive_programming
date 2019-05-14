14b6c77ed28126c9f4ad23ab83bae62ab2ff5ef9
fishy15 <aaryan.prakash3.14@gmail.com>
2019-05-14 13:31:51 -0700

    reachable

---
+++ 555_div3/reachable.cpp
@@ -0,0 +1,77 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+using namespace std;
+
+set<int> visited;
+int n;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n;
+    while (visited.find(n) == visited.end()) {
+        visited.insert(n);
+        n++;
+        while (n % 10 == 0 && n > 0) {
+            n /= 10;
+        }
+    }
+
+    cout << visited.size() << '\n';
+
+    return 0;
+}
