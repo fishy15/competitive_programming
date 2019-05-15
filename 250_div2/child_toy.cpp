fe18455b05c01066abd6bdb7a8e837a12814a627
fishy15 <aaryan.prakash3.14@gmail.com>
2019-05-15 10:23:03 -0700

    child toy

---
+++ 250_div2/child_toy.cpp
@@ -0,0 +1,82 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+#define MAXN 1000
+
+using namespace std;
+
+int n, m;
+int ans;
+int arr[MAXN];
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x, y; cin >> x >> y;
+        x--; y--;
+        ans += min(arr[x], arr[y]);
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
