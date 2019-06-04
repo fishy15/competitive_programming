a8513d77cc7436eca0f2871d50913614ea813551
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-04 16:50:03 -0700

    new year and rating

--- cf/bye2016/rating.cpp
+++ cf/bye2016/rating.cpp
@@ -55,20 +55,41 @@
 #define ll long long
 #define eps 1e-8
 #define MOD 10000007
+#define INF 0x3f3f3f3f
 
 using namespace std;
 
 int n;
 vector<pair<int, int>> vals;
-
-int 
+int diff;
+int min_1 = INF;
+int max_2 = -INF;
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         int x, y; cin >> x >> y;
-        vals.push_back{x, y};
+        vals.push_back({x, y});
     }
 
+    for (int i = 0; i < n; i++) {
+        if (vals[i].second == 1) {
+            min_1 = min(min_1, diff);
+        } else {
+            max_2 = max(max_2, diff);
+        }
 
+        diff += vals[i].first;
+    }
+
+    if (min_1 <= max_2) {
+        cout << "Impossible\n";
+    } else if (max_2 == -INF) {
+        cout << "Infinity\n";
+    } else {
+        //cout << min_1 << ' ' << max_2 << ' ' << diff << '\n';
+        cout << 1899 - max_2 + diff << '\n';
+    }
+
+    return 0;
 }
