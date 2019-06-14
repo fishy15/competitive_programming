4147d169659a36516ee361d047ce1175dbd02117
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-13 22:52:44 -0700

    alternative thinking

---
+++ cf/334_div1/alt_think.cpp
@@ -0,0 +1,87 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+using namespace std;
+
+int n;
+string s;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n >> s;
+
+    char cur = s[0];
+
+    int len = 1;
+    int doub = 0;
+    for (int i = 1; i < n; i++) {
+        if (s[i] != cur) {
+            len++;
+            cur = s[i];
+        }
+
+        if (s[i - 1] == s[i]) {
+            doub++;
+        }
+    }
+
+    cout << len + min(doub, 2) << '\n';
+
+    return 0;
+}
