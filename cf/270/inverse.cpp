d4e02ceffedbee6b3b9679ea560759951952571c
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-19 18:32:49 -0700

    cses + finish write code

---
+++ cf/270/inverse.cpp
@@ -0,0 +1,94 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+#define MAXN 2000
+
+using namespace std;
+
+int n;
+int dist[MAXN][MAXN];
+vector<pair<int, int>> graph;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+        }
+    }
+
+    graph = {{0, -1}};
+
+    for (int i = 0; i < n; i++) {
+        if (dist[i][i] != 0) {
+            cout << "NO\n";
+            return 0;
+        }
+
+        for (int j = i + 1; j < n; j++) {
+            if (dist[i][j] != dist[j][i]) {
+                cout << "NO\n";
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ cses/intro/missing.cpp
@@ -0,0 +1,86 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 10000007
+
+#define MAXN 200000
+
+using namespace std;
+
+int n;
+int arr[MAXN];
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n - 1);
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i - 1] != i) {
+            cout << i << '\n';
+            return 0;
+        }
+    }
+
+    cout << n << '\n';
+
+    return 0;
+}
