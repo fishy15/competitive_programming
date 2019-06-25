565cee775a748c9d659de6258b01a4e89f42545b
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-24 19:29:51 -0700

    increasing array

---
+++ cses/intro/increasing.cpp
@@ -0,0 +1,93 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+// :pray: :coolguy:
+// :pray: :imax:
+// :pray: :howlet:
+// :pray: :darren:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 200000 
+
+using namespace std;
+
+int n;
+int nums[MAXN];
+ll ans;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (nums[i] < nums[i - 1]) {
+            ans += nums[i - 1] - nums[i];
+            nums[i] = nums[i - 1];
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
