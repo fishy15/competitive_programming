2ce83503d8c60d93fe0f6ad787754601f8f0b160
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-25 22:08:44 -0700

    book shop

---
+++ cses/dp/book_shop.cpp
@@ -0,0 +1,100 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+// :pray: :coolguy:
+// :pray: :imax:
+// :pray: :howlet:
+// :pray: :darren:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+int n, x;
+int h[1000];
+int s[1000];
+int dp[100001];
+int ans;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = x; j >= 0; j--) {
+            if (j - h[i] >= 0) {
+                dp[j] = max(dp[j], dp[j - h[i]] + s[i]);
+                ans = max(ans, dp[j]);
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
