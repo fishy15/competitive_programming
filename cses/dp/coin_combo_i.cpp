2cf64035590f653f61f15b0dd26e69bce572a2b9
fishy15 <aaryan.prakash3.14@gmail.com>
2019-06-25 20:42:34 -0700

    coin combinations i

---
+++ cses/dp/coin_combo_i.cpp
@@ -0,0 +1,101 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+// :pray: :coolguy:
+// :pray: :imax:
+// :pray: :howlet:
+// :pray: :darren:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+int n, x;
+vector<int> coins;
+int dp[MAXN + 1];
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n >> x;
+
+    dp[0] = 1;
+
+    for (int i = 0; i < n; i++) {
+        int c; cin >> c;
+        coins.push_back(c);
+    }
+
+    for (int i = 1; i <= x; i++) {
+        for (int& c : coins) {
+            if (i - c >= 0) {
+                dp[i] += dp[i - c];
+
+                if (dp[i] > MOD) {
+                    dp[i] -= MOD;
+                }
+            }
+        }
+    }
+
+    cout << dp[x] << '\n';
+    return 0;
+}
