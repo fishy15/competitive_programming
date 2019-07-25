6774dfa6e289f82e31ca9d23493cca25e7c7fd4d
fishy15 <aaryan.prakash3.14@gmail.com>
2019-07-25 09:38:55 -0400

    frog2

---
+++ atcoder/dp/frog2.cpp
@@ -0,0 +1,101 @@
+// :pray: :arrayman:
+// :pray: :summit:
+// :pray: :pusheen:
+// :pray: :prodakcin:
+// :pray: :spacewalker:
+// :pray: :duk:
+// :pray: :gustav:
+// :pray: :tmw:
+// :pray: :eed:
+// :pray: :chicubed:
+// :pray: :arceus:
+// :pray: :gamegame:
+// :pray: :sinx:
+// :pray: :eyg:
+// :pray: :evan:
+// :pray: :cj:
+// :pray: :steph:
+// :pray: :fatant:
+// :pray: :mathbot:
+// :pray: :dolphin:
+// :pray: :stef:
+// :pray: :geothermal:
+// :pray: :mikey:
+// :pray: :horse:
+// :pray: :snek:
+// :pray: :wayne:
+// :pray: :wu:
+// :pray: :vmaddur:
+// :pray: :dorijanko:
+// :pray: :gaming:
+// :pray: :kassuno:
+// :pray: :hyacinth:
+// :pray: :chilli:
+// :pray: :dhruv:
+// :pray: :philip:
+// :pray: :paiman:
+// :pray: :camel:
+// :pray: :tree:
+// :pray: :tux:
+// :pray: :jony:
+// :pray: :kage:
+// :pray: :ghost:
+// :pray: :aha:
+// :pray: :coolguy:
+// :pray: :imax:
+// :pray: :howlet:
+// :pray: :darren:
+// :pray: :arie: 
+// :pray: :blastman: 
+// :pray: :zephyr: 
+// :pray: :bigc:
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 100000
+
+using namespace std;
+
+int n;
+int k;
+int h[MAXN + 1];
+int dp[MAXN + 1];
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+
+    for (int i = 1; i < n; i++) {
+        dp[i] = dp[i - 1] + abs(h[i] - h[i - 1]);
+
+        for (int d = 2; d <= k; d++) {
+            if (i - d >= 0) {
+                dp[i] = min(dp[i], dp[i - d] + abs(h[i] - h[i - d]));
+            }
+        }
+    }
+
+    cout << dp[n - 1] << '\n';
+
+    return 0;
+}
