deacd8dd54aa1727d9bea94fc660c08bdf6f9d32
fishy15 <aaryan.prakash3.14@gmail.com>
2019-08-28 01:08:12 -0700

    periodic strings

--- uva/455.cpp
+++ uva/455.cpp
@@ -1,69 +1,9 @@
-// :pray: :arrayman:
-// :pray: :summit:
-// :pray: :pusheen:
-// :pray: :prodakcin:
-// :pray: :spacewalker:
-// :pray: :duk:
-// :pray: :gustav:
-// :pray: :tmw:
-// :pray: :eed:
-// :pray: :chicubed:
-// :pray: :arceus:
-// :pray: :gamegame:
-// :pray: :sinx:
-// :pray: :eyg:
-// :pray: :evan:
-// :pray: :cj:
-// :pray: :steph:
-// :pray: :fatant:
-// :pray: :mathbot:
-// :pray: :dolphin:
-// :pray: :stef:
-// :pray: :geothermal:
-// :pray: :mikey:
-// :pray: :horse:
-// :pray: :snek:
-// :pray: :wayne:
-// :pray: :wu:
-// :pray: :vmaddur:
-// :pray: :dorijanko:
-// :pray: :gaming:
-// :pray: :kassuno:
-// :pray: :hyacinth:
-// :pray: :chilli:
-// :pray: :dhruv:
-// :pray: :philip:
-// :pray: :paiman:
-// :pray: :camel:
-// :pray: :tree:
-// :pray: :tux:
-// :pray: :jony:
-// :pray: :kage:
-// :pray: :ghost:
-// :pray: :aha:
-// :pray: :coolguy:
-// :pray: :imax:
-// :pray: :xyzyzl:
-// :pray: :darren:
-// :pray: :arie: 
-// :pray: :blastman: 
-// :pray: :zephyr: 
-// :pray: :bigc:
-// :pray: :aeren: 
-// :pray: :proactiveman:
-// :pray: :jkiplo:
-// :pray: :suneet:
-// :pray: :teh:
-// :pray: :numb:
-// :pray: :sriraamaster:
-// :pray: :hwl:
-// :pray: :dajeff:
-// :pray: :uwunoob:
-// :pray: :rythm:
+// :pray: :fishy:
 
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <utility>
 #include <map>
@@ -83,8 +23,48 @@
 
 using namespace std;
 
+void solve() {
+    string s; cin >> s;
+    int n = (int)(s.size());
+
+    vector<int> z(n);
+    int l = 0;
+    int r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i <= r) {
+            z[i] = min(r - i + 1, z[i - l]);
+        }
+
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+
+    for (int i = 1; i <= n / 2; i++) {
+        if (z[n - i] == i && z[i] == n - i && n % i == 0) {
+            cout << i << '\n';
+            return;
+        }
+    }
+
+    cout << n << '\n';
+}
+
 int main() {
     cin.tie(0); ios::sync_with_stdio(0);
+
+    int q; cin >> q;
+    while (q--) {
+        solve();
+        if (q > 0) {
+            cout << '\n';
+        }
+    }
 
     return 0;
 }
