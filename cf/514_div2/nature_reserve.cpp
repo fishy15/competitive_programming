3a7e0c281f6b52d28b3a00695884a94ce231abe6
fishy15 <aaryan.prakash3.14@gmail.com>
2019-09-07 17:02:27 -0700

    nature reserve

--- cf/514_div2/nature_reserve.cpp
+++ cf/514_div2/nature_reserve.cpp
@@ -1,46 +1,9 @@
-// :pray: :arrayman:
-// :pray: :summit:
-// :pray: :pusheen:
-// :pray: :prodakcin:
-// :pray: :spacewalker:
-// :pray: :duk:
-// :pray: :gustav:
-// :pray: :tmw:
-// :pray: :eed:
-// :pray: :chicubed:
-// :pray: :arceus:
-// :pray: :gamegame:
-// :pray: :sinx:
-// :pray: :eyg:
-// :pray: :evan:
-// :pray: :cj:
-// :pray: :steph:
-// :pray: :fatant:
-// :pray: :mathbot:
-// :pray: :dolphin:
-// :pray: :stef:
-// :pray: :geothermal:
-// :pray: :mikey:
-// :pray: :horse:
-// :pray: :snek:
-// :pray: :wayne:
-// :pray: :wu:
-// :pray: :vmaddur:
-// :pray: :dorijanko:
-// :pray: :gaming:
-// :pray: :kassuno:
-// :pray: :hyacinth:
-// :pray: :chilli:
-// :pray: :dhruv:
-// :pray: :philip:
-// :pray: :paiman:
-// :pray: :camel:
-// :pray: :tree:
-// :pray: :tux:
+// :pray: :fishy:
 
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <utility>
 #include <map>
@@ -50,26 +13,47 @@
 
 #define ll long long
 #define eps 1e-8
-#define MOD 10000007
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
 
 using namespace std;
 
-ll n;
-vector<pair<ll, ll>> pts;
+int n;
+vector<pair<double, double>> pts;
+
+double calc(double x) {
+    double max_r = 0;
+    for (int i = 0; i < n; i++) {
+        double r = (x - pts[i].first) * (x - pts[i].first) + pts[i].second * pts[i].second;
+        r /= 2;
+        r /= pts[i].second;
+        max_r = max(r, max_r);
+    }
+
+    return max_r;
+}
 
 int main() {
     cin.tie(0); ios::sync_with_stdio(0);
 
     cin >> n;
-
     bool pos = false;
     bool neg = false;
-    for (ll i = 0; i < n; i++) {
-        ll x, y; cin >> x >> y;
-        if (y > 0) pos = true;
-        else neg = true; 
 
-        pts.push_back({x, y});
+    for (int i = 0; i < n; i++) {
+        double x, y; cin >> x >> y;
+        if (y > 0) {
+            pos = true;
+            pts.push_back({x, y});
+        } else {
+            neg = true;
+            pts.push_back({x, -y});
+        }
     }
 
     if (pos && neg) {
@@ -77,35 +61,30 @@ int main() {
         return 0;
     }
 
-    ll max_height = 0;
-    ll l = 0;
-    ll r = 0;
+    double l = -1e7;
+    double r = 1e7;
+    double ans = INFLL;
 
-    for (pair<ll, ll> p : pts) {
-        if (neg) p.second *= -1;
-        
-        if (p.second > max_height) {
-            max_height = p.second;
-            l = p.first;
-            r = p.first;
-        } else if (p.second == max_height) {
-            l = min(l, p.first);
-            r = max(r, p.second);
+    while (r - l > eps) {
+        double m1 = l + (r - l) / 3;
+        double m2 = r - (r - l) / 3;
+        double r1 = calc(m1);
+        double r2 = calc(m2);
+
+        if (r1 - r2 > eps) {
+            l = m1;
+        } else if (r2 - r1 > eps) {
+            r = m2; 
+        } else {
+            l = m1;
+            r = m2;
         }
+
+        ans = min(ans, r1);
+        ans = min(ans, r2);
     }
 
-    double ans = 0.0;
-    ans = (double)max_height / 2;
-    if (l != r) {
-         double ans1 = (double)(r - l);
-         ans1 = ans1 * ans1;
-         ans1 /= 8;
-         ans += ans1;
-    }
-
-    cout << ans << '\n';
+    cout << fixed << ans << '\n';
 
     return 0;
 }
-
-// TODO: CHANGE TO BINARY SEARCH
