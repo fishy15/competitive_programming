cb0c52a0caa57b296dfb8c6ed402d1ba454d7ba0
fishy15 <aaryan.prakash3.14@gmail.com>
2020-01-01 13:39:49 -0800

    more deletions

--- usaco/contest/dec17/silver/homework/tester.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class tester {
-    public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(2, "fatty");
-        System.out.println(map.get(1));
-    }
-}
