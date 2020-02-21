2bba316814b08814ccd6cecbcee6bfb12666f8fa
fishy15 <aaryan.prakash3.14@gmail.com>
2020-02-21 00:01:54 -0800

    the cow gathering

--- usaco/contest/dec18/plat/gathering.cpp
+++ usaco/contest/dec18/plat/gathering.cpp
@@ -1,7 +1,5 @@
-// :pray: :steph:
-// :pray: :bakekaga:
-
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 #include <array>
@@ -20,15 +18,130 @@
 #define INFLL 0x3f3f3f3f3f3f3f3f
 
 // change if necessary
-#define MAXN 1000000
+#define MAXN 100000
 
 using namespace std;
 
-// check if a solution exists
-// if it does then do range prefix sums thing
+int n, m;
+vector<int> adj[MAXN + 1];
+vector<int> req[MAXN + 1];
+int dp[2 * MAXN + 10];
+int deg[MAXN + 1];
+
+int t = 0;
+int in[MAXN + 1];
+int out[MAXN + 1];
+int ord[2 * MAXN + 10];
+int ans[MAXN + 1];
+int up[MAXN + 1][18];
+
+void dfs(int v, int p) {
+    in[v] = t++;
+
+    up[v][0] = p;
+    for (int i = 1; i < 18; i++) {
+        up[v][i] = up[up[v][i - 1]][i - 1];
+    }
+
+    for (int e : adj[v]) {
+        if (e != p) {
+            dfs(e, v); 
+        }
+    }
+
+    out[v] = t++;
+}
+
+bool anc(int p, int c) {
+    return in[p] <= in[c] && out[c] <= out[p];
+}
 
 int main() {
-    cin.tie(0); ios::sync_with_stdio(0);
+    ifstream fin("gathering.in");
+    ofstream fout("gathering.out");
+
+    fin >> n >> m;
+    for (int i = 0; i < n - 1; i++) {
+        int a, b; fin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        deg[a]++;
+        deg[b]++;
+    }
+
+    dfs(1, 1);
+
+    for (int i = 1; i <= n; i++) {
+        ord[in[i]] = i;
+        ord[out[i]] = -i;
+        int a, b; fin >> a >> b;
+        deg[b]++;
+        req[a].push_back(b);
+        if (anc(a, b)) {
+            dp[in[1]]++;
+            dp[out[1] + 1]--;
+
+            for (int i = 17; i >= 0; i--) {
+                if (!anc(up[b][i], a)) {
+                    b = up[b][i];
+                }
+            }
+
+            dp[in[b]]--;
+            dp[out[b] + 1]++;
+        } else {
+            dp[in[a]]++;
+            dp[out[a] + 1]--;
+        } 
+    }
+
+    for (int i = 0; i <= out[1]; i++) {
+        if (i > 0) {
+            dp[i] += dp[i - 1];
+        }
+
+        if (ord[i] > 0) {
+            ans[ord[i]] = dp[i] == 0 ? 1 : 0;
+        }
+    }
+
+    int cc = 0;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (deg[i] == 1) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        cc++;
+
+        for (int e : adj[x]) {
+            deg[e]--;
+            if (deg[e] == 1) {
+                q.push(e);
+            }
+        }
+
+        for (int e : req[x]) {
+            deg[e]--;
+            if (deg[e] == 1) {
+                q.push(e);
+            }
+        }
+    }
+
+    if (cc < n) {
+        for (int i = 1; i <= n; i++) {
+            fout << "0\n";
+        }
+    } else {
+        for (int i = 1; i <= n; i++) {
+            fout << ans[i] << '\n';
+        }
+    }
 
     return 0;
 }
