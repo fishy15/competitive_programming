69f1a643b914938c963db955aeb1e5ea874a2688
fishy15 <aaryan.prakash3.14@gmail.com>
2020-02-28 09:44:34 -0800

    cleaning up files

--- usaco/contest/jan20/plat/cave.cpp
+++ usaco/contest/jan20/plat/cave.cpp
@@ -18,40 +18,13 @@
 #define INFLL 0x3f3f3f3f3f3f3f3f
 
 // change if necessary
-#define MAXN 1001
+#define MAXN 1000
 
 using namespace std;
 
-struct node {
-    int parent;
-    set<int> child;
-    bool visited;
-};
-
 int n, m;
 string grid[MAXN];
-// graph[row][color]
-node* graph[MAXN][MAXN];
-int color[MAXN][MAXN];
-
-ll calc(int i, int c) {
-    /* cout << '\t' << i << ' ' << c << '\n'; */ if (graph[i][c]->visited) {
-        return 1LL;
-    }
-
-    node* n = graph[i][c];
-    n->visited = true;
-
-    ll ans = 1;
-    for (int c2 : n->child) {
-        ans *= calc(i + 1, c2);
-        ans %= MOD;
-    }
-
-    ans++;
-    ans %= MOD;
-    return ans;
-}
+vector<int> tree[MAXN];
 
 int main() {
     ifstream fin("cave.in");
@@ -60,54 +33,7 @@ int main() {
     fin >> n >> m;
     for (int i = 0; i < n; i++) {
         fin >> grid[i];
-    }
-
-    for (int i = n - 1; i >= 0; i--) {
-        int cnt = 1;
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == '.') {
-                if (grid[i][j - 1] == '.') {
-                    color[i][j] = color[i][j - 1];
-                } else {
-                    color[i][j] = cnt++;
-                    graph[i][color[i][j]] = new node;
-                }
-
-                if (grid[i + 1][j] == '.') {
-                    if (graph[i + 1][color[i + 1][j]]->parent == 0) {
-                        graph[i + 1][color[i + 1][j]]->parent = color[i][j];
-                        graph[i][color[i][j]]->child.insert(color[i + 1][j]);
-                    } else if (graph[i + 1][color[i + 1][j]]->parent != color[i][j]) {
-                        for (int c : graph[i][color[i][j]]->child) {
-                            graph[i][graph[i + 1][color[i + 1][j]]->parent]->child.insert(c);
-                        }
-                        graph[i][color[i][j]] = graph[i][graph[i + 1][color[i + 1][j]]->parent];
-                    }
-                }
-            }
-        }
-    }
-
-    /* for (int i = 0; i < n; i++) { */
-    /*     for (int j = 0; j < m; j++) { */
-    /*         cout << color[i][j] << ' '; */
-    /*     } */
-    /*     cout << '\n'; */
-    /* } */
-
-    ll ans = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == '.') {
-                ans *= calc(i, color[i][j]);
-                ans %= MOD;
-            }
-        }
-    }
-
-    fout << ans << '\n';
+    }    
 
     return 0;
 }
-
