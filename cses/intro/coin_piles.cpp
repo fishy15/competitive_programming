58bd10532095a5c72f35e530f78ae5a9c8a1c56f
fishy15 <aaryan.prakash3.14@gmail.com>
2020-03-22 20:56:50 -0700

    coin piles (cses)

---
+++ cses/intro/coin_piles.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+void solve() {
+    int a, b; cin >> a >> b;
+    if (a > b) swap(a, b);
+    if ((a + b) % 3 != 0 || 2 * a < b) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
