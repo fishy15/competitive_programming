e3f142cdecc2be44ceb631270aa8294947e23528
fishy15 <aaryan.prakash3.14@gmail.com>
2020-04-13 01:28:56 -0700

    stick lengths

---
+++ cses/sort_search/stick_lengths.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 200000
+
+using namespace std;
+
+int n;
+int nums[MAXN];
+ll ans;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + n);
+    ll m = nums[n / 2];
+
+    for (int i = 0; i < n; i++) {
+        ans += abs(nums[i] - m);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
