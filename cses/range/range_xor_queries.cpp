1ced8cbc579df366a25d6256a6230a2302474637
fishy15 <aaryan.prakash3.14@gmail.com>
2020-06-02 13:58:24 -0700

    range xor queries (cses)

---
+++ cses/range/range_xor_queries.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 200010
+
+using namespace std;
+
+int n, q;
+ll pre[MAXN];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> pre[i];
+        pre[i] ^= pre[i - 1];
+    }
+
+    for (int i = 0; i < q; i++) {
+        int a, b; cin >> a >> b;
+        cout << (pre[b] ^ pre[a - 1]) << '\n';
+    }
+
+    return 0;
+}
