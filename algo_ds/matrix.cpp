afde9d91fb579ed366e169189d910cd0c84f6204
fishy15 <aaryan.prakash3.14@gmail.com>
2020-06-17 00:01:42 -0700

    edit matrix mult

--- algo_ds/matrix.cpp
+++ algo_ds/matrix.cpp
@@ -1,36 +1,31 @@
-vector<vector<ll>> matrix_mult(vector<vector<ll>> m1, vector<vector<ll>> m2) {
-    vector<vector<ll>> ans;
-
-    for (ll i = 0; i < m1.size(); i++) {
-        vector<ll> row;
-        for (ll j = 0; j < m2[0].size(); j++) {
-            ll sum = 0;
-            for (ll a = 0; a < m1[0].size(); a++) {
-                sum += m1[i][a] * m2[a][j];
-
-                // include if mod is necessary
-                sum %= MOD;
+struct M {
+    const static int n = 100;
+    ll m[n][n];
+    M() { memset(m, 0, sizeof m); }
+    M operator*(const M& m2) {
+        M res;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int a = 0; a < n; a++) {
+                    res.m[i][j] += m[i][a] * m2.m[a][j];
+                    if (res.m[i][j] >= MOD) res.m[i][j] %= MOD;
+                }
             }
-            
-            row.push_back(sum);
         }
-
-        ans.push_back(row);
+        return res;
     }
-
-    return ans;
-}
-
-vector<vector<ll>> matrix_expo(vector<vector<ll>> m, ll n) {
-    if (n == 1) {
+    static M pow(M &m, int e) {
+        M res = I();
+        while (e) {
+            if (e & 1) res = res * m;
+            m = m * m;
+            e >>= 1;
+        }
+        return res;
+    }
+    static M I() {
+        M m;
+        for (int i = 0; i < maxn; i++) m.m[i][i] = 1;
         return m;
     }
-
-    vector<vector<ll>> temp = matrix_expo(m, n / 2);
-
-    if (n % 2 == 0) {
-        return matrix_mult(temp, temp);
-    }
-
-    return matrix_mult(matrix_mult(temp, temp), m);
-}
+};
