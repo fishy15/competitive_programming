6605497b2903384134941b0b81ea34bb38ff5025
fishy15 <aaryan.prakash3.14@gmail.com>
2020-07-01 15:55:40 -0700

    distributing apples (cses)

---
+++ cses/math/distributing_apples.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define ll long long
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 2000010
+
+using namespace std;
+
+int n, m;
+ll f[MAXN];
+ll finv[MAXN];
+
+ll modpow(ll n, ll e) {
+    ll res = 1;
+    while (e) {
+        if (e & 1) res = res * n % MOD;
+        n = n * n % MOD;
+        e >>= 1;
+    }
+    return res;
+}
+
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    f[0] = 1;
+    finv[0] = 1;
+    for (int i = 1; i < MAXN; i++) {
+        f[i] = f[i - 1] * i % MOD;
+        finv[i] = modpow(f[i], MOD - 2);
+    }
+
+    cin >> n >> m;
+    ll a = n + m - 1;
+    ll b = n - 1;
+    cout << f[a] * finv[b] % MOD * finv[a - b] % MOD << '\n';
+
+    return 0;
+}
