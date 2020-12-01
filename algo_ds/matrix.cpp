ca9a0d82f362a213d10589f7d04dcc0df4a61a56
fishy15 <aaryan.prakash3.14@gmail.com>
2020-11-30 17:16:00 -0800

    update matrix template

--- algo_ds/matrix.cpp
+++ algo_ds/matrix.cpp
@@ -2,19 +2,20 @@ struct M {
     const static int n = 100;
     ll m[n][n];
     M() { memset(m, 0, sizeof m); }
-    M operator*(const M& m2) {
+    M operator*(const M &m2) {
         M res;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int a = 0; a < n; a++) {
-                    res.m[i][j] += m[i][a] * m2.m[a][j];
-                    if (res.m[i][j] >= MOD) res.m[i][j] %= MOD;
+                    res.m[i][j] += m[i][a] * m2[a][j];
+                    if (res.m[i][j] >= MOD) res[i][j] %= MOD;
                 }
             }
         }
         return res;
     }
-    ll* operator[](const int &x) { return m[x]; }
+    ll *operator[](const int &x) { return m[x]; }
+    const ll *operator[](const int &x) const {return m[x]; }
     static M pow(M &m, ll e) {
         M res = I();
         while (e) {
@@ -26,7 +27,7 @@ struct M {
     }
     static M I() {
         M m;
-        for (int i = 0; i < maxn; i++) m.m[i][i] = 1;
+        for (int i = 0; i < n; i++) m[i][i] = 1;
         return m;
     }
 };
