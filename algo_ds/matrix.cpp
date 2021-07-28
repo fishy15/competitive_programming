db16dbba71a5436687f1d67f0afa17366e03119b
fishy15 <aaryan.prakash3.14@gmail.com>
2021-07-28 01:31:59 -0700

    update matrix template and add modint template

--- algo_ds/matrix.cpp
+++ algo_ds/matrix.cpp
@@ -1,24 +1,22 @@
 struct M {
-    const static int n = 100;
-    ll m[n][n];
-    M() { memset(m, 0, sizeof m); }
+    const static int n = 20;
+    mi m[n][n]; 
     M operator*(const M &m2) {
         M res;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int a = 0; a < n; a++) {
-                    res.m[i][j] += m[i][a] * m2[a][j];
-                    if (res.m[i][j] >= MOD) res[i][j] %= MOD;
+                    res[i][j] += m[i][a] * m2[a][j];
                 }
             }
         }
         return res;
     }
-    ll *operator[](const int &x) { return m[x]; }
-    const ll *operator[](const int &x) const {return m[x]; }
-    static M pow(M &m, ll e) {
+    mi *operator[](const int x) { return m[x]; }
+    const mi *operator[](const int x) const { return m[x]; }
+    static M pow(M m, ll e) {
         M res = I();
-        while (e) {
+        while (e > 0) {
             if (e & 1) res = res * m;
             m = m * m;
             e >>= 1;
---
+++ algo_ds/modint.cpp
@@ -0,0 +1,31 @@
+struct mi {
+    int v;
+    mi() : mi(0) {}
+    mi(int _v) : v(_v) {
+        if (v >= MOD) v -= MOD;
+        if (v < 0) v += MOD;
+    }
+    mi(ll _v) : mi((int)(_v % MOD)) {}
+    mi operator+(const mi &m2) const { return mi(v + m2.v); }
+    mi operator-(const mi &m2) const { return mi(v - m2.v); }
+    mi operator*(const mi &m2) const { return mi((ll) v * m2.v); }
+    mi operator/(const mi &m2) const { return mi((ll) v * m2.inv().v); }
+    mi &operator+=(const mi &m2) { return *this = *this + m2; }
+    mi &operator-=(const mi &m2) { return *this = *this - m2; }
+    mi &operator*=(const mi &m2) { return *this = *this * m2; }
+    mi &operator/=(const mi &m2) { return *this = *this / m2; }
+    mi pow(ll e) const {
+        mi res = 1;
+        mi n = *this;
+        while (e > 0) {
+            if (e & 1) res *= n;
+            n *= n;
+            e >>= 1;
+        }
+        return res;
+    }
+    mi inv() const {
+        return pow(MOD - 2);
+    }
+};
+
