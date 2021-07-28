43dba7219979d8b7257b39ef8ce334a879814953
fishy15 <aaryan.prakash3.14@gmail.com>
2021-07-27 22:27:10 -0700

    template y combinator

---
+++ algo_ds/y_combinator.cpp
@@ -0,0 +1,16 @@
+// For making lambda functions recursive
+// The first parameter of the lambda function must be self
+// The trailing return type must be specified
+template<typename F>
+struct y_combinator_result {
+    F f;
+    template<typename T> explicit y_combinator_result(T &&f) : f(forward<T>(f)) {}
+    template<typename... Args> decltype(auto) operator()(Args &&...args) {
+        return f(ref(*this), forward<Args>(args)...);
+    }
+};
+
+template<typename F>
+decltype(auto) y_combinator(F &&f) {
+    return y_combinator_result<decay_t<F>>(forward<F>(f));
+}
