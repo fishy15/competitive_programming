6cbc7763d09f9a496a9230d0627e15ac66330905
= <aaryan.prakash3.14@gmail.com>
2023-02-03 17:18:51 -0600

    Update HLD Template

--- algo_ds/hld.cpp
+++ algo_ds/hld.cpp
@@ -1,48 +1,71 @@
-vector<int> adj[MAXN];
-int par[MAXN];
-int sz[MAXN];
-int in[MAXN];
-int head[MAXN];
-int d[MAXN];
-int t;
+template<bool op_edges = false>
+struct hld {
+    vector<vector<int>> adj;
+    vector<int> par;
+    vector<int> sz;
+    vector<int> in;
+    vector<int> head;
+    vector<int> d;
 
-void dfs_size(int v, int p) {
-    sz[v] = 1;
-    for (int &e : adj[v]) {
-        if (e != p) {
-            d[e] = d[v] + 1;
-            par[e] = v;
-            dfs_size(e, v);
-            sz[v] += sz[e];
-            if (sz[e] > sz[adj[v][0]] || adj[v][0] == p) swap(e, adj[v][0]);
-        }
+    hld(int n) : adj(n), par(n), sz(n), in(n), head(n), d(n) {}
+
+    void add_edge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-}
 
-void dfs_hld(int v, int p) {
-    in[v] = t++;
-    for (int e : adj[v]) {
-        if (e != p) {
-            if (e == adj[v][0]) {
-                head[e] = head[v];
-            } else {
-                head[e] = e;
+    void dfs_size(int v = 0, int p = 0) {
+        sz[v] = 1;
+        for (int &e : adj[v]) {
+            if (e != p) {
+                d[e] = d[v] + 1;
+                par[e] = v;
+                dfs_size(e, v);
+                sz[v] += sz[e];
+                if (sz[e] > sz[adj[v][0]] || adj[v][0] == p) swap(e, adj[v][0]);
             }
-            dfs_hld(e, v);
         }
     }
-}
 
-void op_path(int x, int y, function<void(int,int)> op) {
-    while (head[x] != head[y]) {
-        if (d[head[x]] > d[head[y]]) swap(x, y);
-        op(in[head[y]], in[y]);
-        y = par[head[y]];
+    void dfs_hld(int v = 0, int p = 0) {
+        static int t = 0;
+        in[v] = t++;
+        for (int e : adj[v]) {
+            if (e != p) {
+                if (e == adj[v][0]) {
+                    head[e] = head[v];
+                } else {
+                    head[e] = e;
+                }
+                dfs_hld(e, v);
+            }
+        }
     }
-    if (d[x] > d[y]) swap(x, y);
-    op(in[x], in[y]); // if vals in edges, add 1 to in[x] to exclude parent edge
-}
 
-void op_subtree(int x, function<void(int,int)> op) {
-    op(in[x], in[x] + sz[x] - 1); // if vals in edges, add 1 to in[x] to exclude parent edge
-}
+    template<typename F, bool enabled = !op_edges, enable_if_t<enabled, bool> = true>
+    void op_node(int x, F op) {
+        op(in[x]);
+    }
+
+    template<typename F, bool enabled = op_edges, enable_if_t<enabled, bool> = true>
+    void op_edge(int x, int y, F op) {
+        if (d[x] < d[y]) swap(x, y);
+        op(in[x]);
+    }
+
+    template<typename F>
+    void op_path(int x, int y, F op) {
+        while (head[x] != head[y]) {
+            if (d[head[x]] > d[head[y]]) swap(x, y);
+            op(in[head[y]], in[y]);
+            y = par[head[y]];
+        }
+        if (d[x] > d[y]) swap(x, y);
+        op(in[x] + (op_edges ? 1 : 0), in[y]);
+    }
+
+    template<typename F>
+    void op_subtree(int x, F op) { 
+        op(in[x] + (op_edges ? 1 : 0), in[x] + sz[x] - 1);
+    }
+};
--- cses/tree/path_queries_ii.cpp
+++ cses/tree/path_queries_ii.cpp
@@ -59,45 +59,77 @@ struct st {
     }
 } st;
 
-int par[MAXN];
-int in[MAXN];
-int sz[MAXN];
-int head[MAXN];
-int d[MAXN];
-int t;
+template<bool op_edges = false>
+struct hld {
+    vector<vector<int>> adj;
+    vector<int> par;
+    vector<int> sz;
+    vector<int> in;
+    vector<int> head;
+    vector<int> d;
 
-void dfs_size(int v, int p) {
-    sz[v] = 1;
-    for (int &e : adj[v]) {
-        if (e != p) {
-            d[e] = d[v] + 1;
-            par[e] = v;
-            dfs_size(e, v);
-            sz[v] += sz[e];
-            if (sz[e] > sz[adj[v][0]] || adj[v][0] == p) swap(e, adj[v][0]);
+    hld(int n) : adj(n), par(n), sz(n), in(n), head(n), d(n) {}
+
+    void add_edge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void dfs_size(int v = 0, int p = 0) {
+        sz[v] = 1;
+        for (int &e : adj[v]) {
+            if (e != p) {
+                d[e] = d[v] + 1;
+                par[e] = v;
+                dfs_size(e, v);
+                sz[v] += sz[e];
+                if (sz[e] > sz[adj[v][0]] || adj[v][0] == p) swap(e, adj[v][0]);
+            }
         }
     }
-}
 
-void dfs_hld(int v, int p) {
-    in[v] = t++;
-    for (int e : adj[v]) {
-        if (e != p) {
-            head[e] = (e == adj[v][0]) ? head[v] : e;
-            dfs_hld(e, v);
+    void dfs_hld(int v = 0, int p = 0) {
+        static int t = 0;
+        in[v] = t++;
+        for (int e : adj[v]) {
+            if (e != p) {
+                if (e == adj[v][0]) {
+                    head[e] = head[v];
+                } else {
+                    head[e] = e;
+                }
+                dfs_hld(e, v);
+            }
         }
     }
-}
 
-void op_path(int x, int y, function<void(int,int)> op) {
-    while (head[x] != head[y]) {
-        if (d[head[x]] > d[head[y]]) swap(x, y);
-        op(in[head[y]], in[y]);
-        y = par[head[y]];
+    template<typename F, bool enabled = !op_edges, enable_if_t<enabled, bool> = true>
+    void op_node(int x, F op) {
+        op(in[x]);
     }
-    if (d[x] > d[y]) swap(x, y);
-    op(in[x], in[y]);
-}
+
+    template<typename F, bool enabled = op_edges, enable_if_t<enabled, bool> = true>
+    void op_edge(int x, int y, F op) {
+        if (d[x] < d[y]) swap(x, y);
+        op(in[x]);
+    }
+
+    template<typename F>
+    void op_path(int x, int y, F op) {
+        while (head[x] != head[y]) {
+            if (d[head[x]] > d[head[y]]) swap(x, y);
+            op(in[head[y]], in[y]);
+            y = par[head[y]];
+        }
+        if (d[x] > d[y]) swap(x, y);
+        op(in[x] + (op_edges ? 1 : 0), in[y]);
+    }
+
+    template<typename F>
+    void op_subtree(int x, F op) { 
+        op(in[x] + (op_edges ? 1 : 0), in[x] + sz[x] - 1);
+    }
+};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -107,18 +139,29 @@ int main() {
         cin >> val[i];
     }
 
+    hld<> tree(n);
+
     for (int i = 0; i < n - 1; i++) {
         int a, b; cin >> a >> b;
         a--; b--;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        tree.add_edge(a, b);
     }
 
-    dfs_size(0, 0);
-    dfs_hld(0, 0);
+    tree.dfs_size();
+    tree.dfs_hld();
+
+    auto upd_node = [&tree](int v, int val) {
+        tree.op_node(v, [val](int v) { st.upd(v, val); });
+    };
+
+    auto qry_path = [&tree](int x, int y) {
+        int res = 0;
+        tree.op_path(x, y, [&res](int x, int y) { res = max(res, st.qry(x, y)); });
+        return res;
+    };
 
     for (int i = 0; i < n; i++) {
-        st.upd(in[i], val[i]);
+        upd_node(i, val[i]);
     }
 
     while (q--) {
@@ -126,11 +169,12 @@ int main() {
         cin >> t >> a >> b;
 
         if (t == 1) {
-            st.upd(in[a - 1], b);
+            a--;
+            upd_node(a, b);
         } else {
-            int res = 0;
-            op_path(a - 1, b - 1, [&res](int x, int y) { res = max(res, st.qry(x, y)); });
-            cout << res << ' ';
+            a--;
+            b--;
+            cout << qry_path(a, b) << ' ';
         }
     }
     cout << '\n';
