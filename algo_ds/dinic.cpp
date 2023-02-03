985a33dbb781b611eea65319a987f4934cbed558
= <aaryan.prakash3.14@gmail.com>
2023-02-03 14:19:09 -0600

    Update dinic template

--- algo_ds/dinic.cpp
+++ algo_ds/dinic.cpp
@@ -1,62 +1,66 @@
-struct edge {
-    int u, v, f, c;
-    edge(int u, int v, int c) : u(u), v(v), f(0), c(c) {}
+struct dinic {
+    struct edge {
+        int u, v;
+        ll f, c;
+        edge(int u, int v, ll c) : u(u), v(v), f{}, c(c) {}
+    };
+
+    vector<vector<int>> adj;
+    vector<edge> edges;
+    vector<int> level, nxt;
+    int start, end;
+
+    dinic(int n, int start, int end) : adj(n), level(n), nxt(n), start(start), end(end) {}
+
+    void add_edge(int u, int v, ll c) {
+        adj[u].push_back(edges.size());
+        edges.push_back({u, v, c});
+        adj[v].push_back(edges.size());
+        edges.push_back({v, u, 0});
+    }
+
+    bool bfs() {
+        queue<int> q;
+        q.push(start);
+        fill(level.begin(), level.end(), -1);
+        level[start] = 0;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int e : adj[v]) {
+                if (edges[e].c - edges[e].f > 0 && level[edges[e].v] == -1) {
+                    level[edges[e].v] = level[v] + 1;
+                    q.push(edges[e].v);
+                }
+            }
+        }
+        return level[end] != -1;
+    }
+
+    ll dfs(int v, ll cur) {
+        if (!cur) return 0;
+        if (v == end) return cur;
+        for (int &i = nxt[v]; i < (int) adj[v].size(); i++) {
+            int e = adj[v][i];
+            if (level[v] + 1 == level[edges[e].v] && edges[e].c - edges[e].f > 0) {
+                ll w = dfs(edges[e].v, min(cur, edges[e].c - edges[e].f));
+                if (w) {
+                    edges[e].f += w;
+                    edges[e ^ 1].f -= w;
+                    return w;
+                }
+            }
+        }
+        return 0;
+    }
+
+    ll flow() {
+        ll flow = 0;
+        while (true) {
+            if (!bfs()) break;
+            fill(nxt.begin(), nxt.end(), 0);
+            while (ll f = dfs(start, INFLL)) flow += f;
+        }
+        return flow;
+    }
 };
-
-vector<int> adj[MAXN];
-vector<edge> edges;
-int level[MAXN];
-int nxt[MAXN];
-int start, end;
-
-void add_edge(int u, int v, int c) {
-    adj[u].push_back(edges.size());
-    edges.push_back({u, v, c});
-    adj[v].push_back(edges.size());
-    edges.push_back({v, u, 0});
-}
-
-bool bfs() {
-    queue<int> q;
-    q.push(start);
-    memset(level, -1, sizeof level);
-    level[start] = 0;
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-        for (int e : adj[v]) {
-            if (edges[e].c - edges[e].f > 0 && level[edges[e].v] == -1) {
-                level[edges[e].v] = level[v] + 1;
-                q.push(edges[e].v);
-            }
-        }
-    }
-    return level[end] != -1;
-}
-
-int dfs(int v, int cur) {
-    if (!cur) return 0;
-    if (v == end) return cur;
-    for (int& i = nxt[v]; i < (int)adj[v].size(); i++) {
-        int e = adj[v][i];
-        if (level[v] + 1 == level[edges[e].v] && edges[e].c - edges[e].f > 0) {
-            int w = dfs(edges[e].v, min(cur, edges[e].c - edges[e].f));
-            if (w) {
-                edges[e].f += w;
-                edges[e ^ 1].f -= w;
-                return w;
-            }
-        }
-    }
-    return 0;
-}
-
-int dinic() {
-    int flow = 0;
-    while (true) {
-        if (!bfs()) break;
-        memset(nxt, 0, sizeof nxt);
-        while (int f = dfs(start, INF)) flow += f;
-    }
-    return flow;
-}
--- cses/graph/download_speed.cpp
+++ cses/graph/download_speed.cpp
@@ -22,84 +22,87 @@
 
 using namespace std;
 
-int n, m;
-vector<int> adj[MAXN];
-int ind;
-vector<array<int, 4>> edge;
-int level[MAXN];
-int cur[MAXN];
+struct dinic {
+    struct edge {
+        int u, v;
+        ll f, c;
+        edge(int u, int v, ll c) : u(u), v(v), f{}, c(c) {}
+    };
 
-bool bfs() {
-    for (int i = 0; i < n; i++) {
-        level[i] = -1;
-        cur[i] = 0;
+    vector<vector<int>> adj;
+    vector<edge> edges;
+    vector<int> level, nxt;
+    int start, end;
+
+    dinic(int n, int start, int end) : adj(n), level(n), nxt(n), start(start), end(end) {}
+
+    void add_edge(int u, int v, ll c) {
+        adj[u].push_back(edges.size());
+        edges.push_back({u, v, c});
+        adj[v].push_back(edges.size());
+        edges.push_back({v, u, 0});
     }
 
-    queue<pair<int, int>> q;
-    q.push({0, 1});
-
-    while (!q.empty()) {
-        auto t = q.front();
-        q.pop();
-        if (level[t.first] != -1) continue;
-        level[t.first] = t.second;
-        for (int e : adj[t.first]) {
-            if (level[edge[e][1]] == -1 && edge[e][2] > edge[e][3]) {
-                q.push({edge[e][1], t.second + 1});
+    bool bfs() {
+        queue<int> q;
+        q.push(start);
+        fill(level.begin(), level.end(), -1);
+        level[start] = 0;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int e : adj[v]) {
+                if (edges[e].c - edges[e].f > 0 && level[edges[e].v] == -1) {
+                    level[edges[e].v] = level[v] + 1;
+                    q.push(edges[e].v);
+                }
             }
         }
+        return level[end] != -1;
     }
 
-    return level[n - 1] != -1;
-}
+    ll dfs(int v, ll cur) {
+        if (!cur) return 0;
+        if (v == end) return cur;
+        for (int &i = nxt[v]; i < (int) adj[v].size(); i++) {
+            int e = adj[v][i];
+            if (level[v] + 1 == level[edges[e].v] && edges[e].c - edges[e].f > 0) {
+                ll w = dfs(edges[e].v, min(cur, edges[e].c - edges[e].f));
+                if (w) {
+                    edges[e].f += w;
+                    edges[e ^ 1].f -= w;
+                    return w;
+                }
+            }
+        }
+        return 0;
+    }
 
-int dfs(int v, int mincap) {
-    if (mincap == 0) return 0;
-    if (v == n - 1) { 
-        return mincap; 
-    }
-    while (cur[v] < adj[v].size()) {
-        auto &e = edge[adj[v][cur[v]]];
-        int nxt = e[1];
-        if ((nxt != n - 1 && level[v] + 1 != level[nxt]) || e[2] == e[3]) {
-            cur[v]++;
-            continue;
+    ll flow() {
+        ll flow = 0;
+        while (true) {
+            if (!bfs()) break;
+            fill(nxt.begin(), nxt.end(), 0);
+            while (ll f = dfs(start, INFLL)) flow += f;
         }
-        int rem = dfs(nxt, min(mincap, e[2] - e[3]));
-        if (rem == 0) {
-            cur[v]++;
-            continue;
-        }
-        e[3] += rem;
-        edge[adj[v][cur[v]] ^ 1][3] -= rem;
-        cur[v]++;
-        return rem;
+        return flow;
     }
-    return 0;
-}
+};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
+    int n, m;
     cin >> n >> m;
+
+    dinic d(n, 0, n - 1);
     for (int i = 0; i < m; i++) {
         int a, b, c; cin >> a >> b >> c;
         a--; b--;
-        adj[a].push_back(ind++);
-        edge.push_back({a, b, c, 0});
-        adj[b].push_back(ind++);
-        edge.push_back({b, a, 0, 0});
+        d.add_edge(a, b, c);
     }
 
-    ll flow = 0;
-    while (true) {
-        if (!bfs()) break;
-        while (int cur = dfs(0, INF)) {
-            flow += cur;
-        }
-    }
-
-    cout << flow << '\n';
+    cout << d.flow() << '\n';
 
     return 0;
 }
