91b257b097164e8f05f2f3af1a8b9c667b4f84a4
= <aaryan.prakash3.14@gmail.com>
2023-05-13 20:31:25 -0700

    Another Game (CSES)

---
+++ cses/math/another_game.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int odd_count = count_if(nums.begin(), nums.end(), [](int x) { return x % 2 == 1; });
+    if (odd_count > 0) {
+        cout << "first\n";
+    } else {
+        cout << "second\n";
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
