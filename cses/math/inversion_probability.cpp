54972b79dbb9c7faef9b14e26fdd2e8e73f33020
= <aaryan.prakash3.14@gmail.com>
2023-05-13 20:28:01 -0700

    Inversion Probability (CSES)

--- cses/math/inversion_probability.cpp
+++ cses/math/inversion_probability.cpp
@@ -9,8 +9,10 @@
 #include <queue>
 #include <set>
 #include <cmath>
+#include <algorithm>
 
 #define ll long long
+#define ld long double
 #define eps 1e-8
 #define MOD 1000000007
 
@@ -34,17 +36,24 @@ int main() {
         cin >> r[i];
     }
 
+    vector<ld> terms;
+
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             for (int x = 1; x <= r[i]; x++) {
                 if (x > r[j]) {
-                    ans += 1.0 / r[i];
+                    terms.push_back((ld) 1.0 / r[i]);
                 } else {
-                    ans += (x - 1.0) / r[j] * 1.0 / r[i];
+                    terms.push_back((ld) (x - 1.0) / r[j] * 1.0 / r[i]);
                 }
             }
         }
     }
+
+    sort(terms.begin(), terms.end());
+    
+    ld ans = 0;
+    for (auto x : terms) ans += x;
 
     cout << fixed << setprecision(6) << ans << '\n';
 
