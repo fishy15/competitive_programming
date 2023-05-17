9c895ea0b88aaf085b9fe340593c9ce648d9366c
= <aaryan.prakash3.14@gmail.com>
2023-05-16 18:57:27 -0700

    Fix warning with Y Combinator

--- algo_ds/y_combinator.cpp
+++ algo_ds/y_combinator.cpp
@@ -4,13 +4,13 @@
 template<typename F>
 struct y_combinator_result {
     F f;
-    template<typename T> explicit y_combinator_result(T &&f) : f(forward<T>(f)) {}
+    template<typename T> explicit y_combinator_result(T &&f) : f(std::forward<T>(f)) {}
     template<typename... Args> decltype(auto) operator()(Args &&...args) {
-        return f(ref(*this), forward<Args>(args)...);
+        return f(ref(*this), std::forward<Args>(args)...);
     }
 };
 
 template<typename F>
 decltype(auto) y_combinator(F &&f) {
-    return y_combinator_result<decay_t<F>>(forward<F>(f));
+    return y_combinator_result<decay_t<F>>(std::forward<F>(f));
 }
