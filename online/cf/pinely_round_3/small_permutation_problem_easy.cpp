069c54d0dd582eb4048641d2035063bc83fb0c85
= <38575855+fishy15@users.noreply.github.com>
2024-01-04 02:05:26 -0800

    Small Permutation Problem (Easy)

---
+++ online/cf/pinely_round_3/small_permutation_problem_easy.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 998244353
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+struct mi {
+    int v;
+    mi() : mi(0) {}
+    mi(int _v) : v(_v) {
+        if (v >= MOD) v -= MOD;
+        if (v < 0) v += MOD;
+    }
+    mi(ll _v) : mi((int)(_v % MOD)) {}
+    mi operator+(const mi &m2) const { return mi(v + m2.v); }
+    mi operator-(const mi &m2) const { return mi(v - m2.v); }
+    mi operator*(const mi &m2) const { return mi((ll) v * m2.v); }
+    mi operator/(const mi &m2) const { return mi((ll) v * m2.inv().v); }
+    mi &operator+=(const mi &m2) { return *this = *this + m2; }
+    mi &operator-=(const mi &m2) { return *this = *this - m2; }
+    mi &operator*=(const mi &m2) { return *this = *this * m2; }
+    mi &operator/=(const mi &m2) { return *this = *this / m2; }
+    mi pow(ll e) const {
+        mi res = 1;
+        mi n = *this;
+        while (e > 0) {
+            if (e & 1) res *= n;
+            n *= n;
+            e >>= 1;
+        }
+        return res;
+    }
+    mi inv() const {
+        return pow(MOD - 2);
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (auto &x : nums) {
+        cin >> x;
+    }
+
+    mi ans = 1;
+    int unassigned = 0;
+
+    int p = 0;
+    for (auto x : nums) {
+        int d = x - p;
+        if (d == 0) {
+            // will be assigned later to some higher value
+            unassigned++;
+        } else if (d == 1) {
+            // current value assigned here or some unassigned
+            ans *= 2 * unassigned + 1; 
+        } else if (d == 2) {
+            // current must be assigned to some previous value
+            // some previous value must be assigned here
+            ans *= mi(unassigned) * unassigned;
+            unassigned--;
+        } else {
+            cout << "0\n";
+            return;
+        }
+        p = x;
+    }
+
+    if (nums.back() == n) {
+        cout << ans.v << '\n';
+    } else {
+        cout << "0\n";
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
