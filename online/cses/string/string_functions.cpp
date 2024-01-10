05a83b0e6a486d546ddb3a4efb617477bc237ca1
= <38575855+fishy15@users.noreply.github.com>
2024-01-10 15:00:49 -0800

    String Functions

---
+++ online/cses/string/string_functions.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+vector<int> z(const string &s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
+
+vector<int> pi(const string &s) {
+    int n = s.size();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+        if (s[i] == s[j]) {
+            j++;
+        }
+        pi[i] = j;
+    }
+    return pi;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+
+    for (auto x : z(s)) {
+        cout << x << ' ';
+    }
+    cout << '\n';
+
+    for (auto x : pi(s)) {
+        cout << x << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
