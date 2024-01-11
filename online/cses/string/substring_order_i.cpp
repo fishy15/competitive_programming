aa61b7a528e15f55945c37b0aab41e0121f5e9a5
= <38575855+fishy15@users.noreply.github.com>
2024-01-10 18:33:32 -0800

    Substring Order I

---
+++ online/cses/string/substring_order_i.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+vector<int> suffix_array(const string &s) {
+    int n = s.size();
+    vector<int> sa(n), c(n);
+    for (int i = 0; i < n; i++) {
+        sa[i] = n - i - 1;
+        c[i] = s[i];
+    }
+
+    stable_sort(sa.begin(), sa.end(), [&s](int i, int j) { return s[i] < s[j]; });
+
+    for (int len = 1; len < n; len *= 2) {
+        vector<int> old_c(c), idx(n), old_sa(sa);
+        iota(idx.begin(), idx.end(), 0);
+
+        for (int i = 0; i < n; i++) {
+            bool same = i > 0 && sa[i - 1] + len < n
+                && old_c[sa[i - 1]] == old_c[sa[i]]
+                && old_c[sa[i - 1] + len / 2] == old_c[sa[i] + len / 2];
+            c[sa[i]] = same ? c[sa[i - 1]] : i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int loc = old_sa[i] - len;
+            if (loc >= 0) {
+                sa[idx[c[loc]]++] = loc;
+            }
+        }
+    }
+
+    return sa;
+}
+
+vector<int> find_lcp(const string &s, const vector<int> &sa) {
+    int n = s.size();
+    vector<int> rank(n);
+    for (int i = 0; i < n; i++) {
+        rank[sa[i]] = i;
+    }
+
+    int len = 0;
+    vector<int> lcp(n - 1);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            len = 0;
+        } else {
+            int j = sa[rank[i] + 1];
+            while (max(i, j) + len < n && s[i + len] == s[j + len]) len++;
+            lcp[rank[i]] = len;
+            if (len > 0) len--;
+        }
+    }
+
+    return lcp;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+
+    ll k;
+    cin >> k;
+    k--;
+    
+    auto sa = suffix_array(s);
+    auto lcp = find_lcp(s, sa);
+
+    int n = s.size();
+    ll l = 0;
+    for (int i = 0; i < n; i++) {
+        auto lcp_prev = i == 0 ? 0 : lcp[i - 1];
+        auto new_substrs = (n - sa[i]) - lcp_prev;
+        if (k < l + new_substrs) {
+            int len = lcp_prev + (k - l + 1);
+            cout << s.substr(sa[i], len) << '\n';
+            return 0;
+        } else {
+            l += new_substrs;
+        }
+    }
+
+    return 0;
+}
