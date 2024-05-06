98f68c2cc73c33b779f8287518872ec080313bc4
= <38575855+fishy15@users.noreply.github.com>
2024-05-06 12:03:28 -0500

    Go Home

---
+++ online/atcoder/arc070/go_home.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+// change if necessary
+#define MAXN 1000000
+
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int x;
+    cin >> x;
+
+    int sum = 0;
+    int iter = 1;
+    while (sum < x) {
+        sum += iter;
+        iter++;
+    }
+
+    cout << iter - 1 << '\n';
+
+    return 0;
+}
