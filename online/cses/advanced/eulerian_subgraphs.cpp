e8ccaea3c05d6f73404368b0333b1c83288f7998
= <38575855+fishy15@users.noreply.github.com>
2024-08-30 00:53:37 -0500

    Eulerian Subgraphs (CSES)

---
+++ online/cses/advanced/eulerian_subgraphs.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+struct mi {
+    int v;
+    mi() : mi(0) {}
+    mi(int _v) : v(_v) {
+        if (v >= MOD) v -= MOD;
+        if (v < 0) v += MOD;
+    }
+    mi(ll _v) : mi((int)(_v % MOD)) {}
+    mi operator+(const mi &m2) const { return mi(v + m2.v); }
+    mi operator-(const mi &m2) const { return mi(v - m2.v); }
+    mi operator*(const mi &m2) const { return mi((ll) v * m2.v); }
+    mi operator/(const mi &m2) const { return mi((ll) v * m2.inv().v); }
+    mi &operator+=(const mi &m2) { return *this = *this + m2; }
+    mi &operator-=(const mi &m2) { return *this = *this - m2; }
+    mi &operator*=(const mi &m2) { return *this = *this * m2; }
+    mi &operator/=(const mi &m2) { return *this = *this / m2; }
+    mi pow(ll e) const {
+        mi res = 1;
+        mi n = *this;
+        while (e > 0) {
+            if (e & 1) res *= n;
+            n *= n;
+            e >>= 1;
+        }
+        return res;
+    }
+    mi inv() const {
+        return pow(MOD - 2);
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector adj(n, vector<int>());
+    rep(i, 0, m) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    vector<int> vis(n);
+    int back_count = 0;
+    auto dfs = [&](auto &&self, int v, int p) -> void {
+        vis[v] = 1;
+        for (auto e : adj[v]) {
+            if (e != p) {
+                if (vis[e] == 0) {
+                    self(self, e, v);
+                } else if (vis[e] == 1) {
+                    back_count++;
+                }
+            }
+        }
+        vis[v] = 2;
+    };
+
+    rep(i, 0, n) {
+        if (!vis[i]) {
+            dfs(dfs, i, i);
+        }
+    }
+
+    cout << mi(2).pow(back_count).v << '\n';
+
+    return 0;
+}
