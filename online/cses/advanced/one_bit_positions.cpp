ad543582e24999d16131758e63732f7bb5f57f33
= <38575855+fishy15@users.noreply.github.com>
2024-10-18 14:37:01 -0500

    One Bit Positions (CSES)

---
+++ online/cses/advanced/one_bit_positions.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+using vi = vector<int>;
+const ll mod = (119 << 23) + 1, root = 62; // = 998244353
+
+ll modpow(ll b, ll e) {
+    ll ans = 1;
+    for (; e; b = b * b % mod, e /= 2)
+        if (e & 1) ans = ans * b % mod;
+    return ans;
+}
+
+// For p < 2^30 there is also e.g. 5 << 25, 7 << 26, 479 << 21
+// and 483 << 21 (same root). The last two are > 10^9.
+typedef vector<ll> vl;
+void ntt(vl &a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vl rt(2, 1);
+    for (static int k = 2, s = 2; k < n; k *= 2, s++) {
+        rt.resize(n);
+        ll z[] = {1, modpow(root, mod >> s)};
+        rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1] % mod;
+    }
+    vi rev(n);
+    rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+            ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
+            a[i + j + k] = ai - z + (z > ai ? mod : 0);
+            ai += (ai + z >= mod ? z - mod : z);
+        }
+}
+vl conv(const vl &a, const vl &b) {
+    if (a.empty() || b.empty()) return {};
+    int s = sz(a) + sz(b) - 1, B = 32 - __builtin_clz(s),
+        n = 1 << B;
+    int inv = modpow(n, mod - 2);
+    vl L(a), R(b), out(n);
+    L.resize(n), R.resize(n);
+    ntt(L), ntt(R);
+    rep(i,0,n)
+        out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
+    ntt(out);
+    return {out.begin(), out.begin() + s};
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+    int n = sz(s);
+
+    vl a(n), b(n);
+    rep(i, 0, n) {
+        if (s[i] == '1') {
+            a[i] = b[n - i - 1] = 1;
+        }
+    }
+
+    auto res = conv(a, b);
+    rep(i, n, 2*n-1) {
+        cout << res[i] << ' ';
+    }
+    cout << '\n';
+
+
+    return 0;
+}
