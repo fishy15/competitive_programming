b28154e77cd5fe7348f448553c43a079198cbfcc
= <38575855+fishy15@users.noreply.github.com>
2024-10-18 14:29:13 -0500

    Apples and Bananas (CSES)

---
+++ online/cses/advanced/apples_and_bananas.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+#include <complex>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+using vi = vector<int>;
+
+typedef complex<double> C;
+typedef vector<double> vd;
+void fft(vector<C>& a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1);  // (^ 10% faster if double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n); rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+    }
+    vi rev(n);
+    rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+            // C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+            auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
+            C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
+            a[i + j + k] = a[i + j] - z;
+            a[i + j] += z;
+        }
+}
+vd conv(const vd& a, const vd& b) {
+    if (a.empty() || b.empty()) return {};
+    vd res(sz(a) + sz(b) - 1);
+    int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vector<C> in(n), out(n);
+    copy(all(a), begin(in));
+    rep(i,0,sz(b)) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in) x *= x;
+    rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int k, n, m;
+    cin >> k >> n >> m;
+
+    vd a(k+1), b(k+1);
+    rep(i, 0, n) {
+        int x;
+        cin >> x;
+        a[x]++;
+    }
+    rep(i, 0, m) {
+        int x;
+        cin >> x;
+        b[x]++;
+    }
+
+    auto res = conv(a, b);
+    rep(i, 2, 2*k+1) {
+        auto x = (ll) (res[i] + 0.5);
+        cout << x << ' ';
+    }
+    cout << endl;
+
+
+    return 0;
+}
