45773eacc68286c99edfade946e952e9131e78d1
= <38575855+fishy15@users.noreply.github.com>
2025-06-16 17:23:00 -0700

    Distinct Values Subsequences (CSES)

---
+++ online/cses/sort_search/distinct_values_subsequences.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 1000000007
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+struct mi {
+    int v;
+    mi() : mi(0) {}
+    mi(int _v) : v(_v) {
+        if (v >= MOD) v -= MOD;
+        if (v < 0) v += MOD;
+    }
+    mi(ll _v) : mi((int)(_v % MOD)) {}
+    mi operator+(const mi &m2) const { return mi(v + m2.v); }
+    mi operator-(const mi &m2) const { return mi(v - m2.v); }
+    mi operator*(const mi &m2) const { return mi((ll) v * m2.v); }
+    mi operator/(const mi &m2) const { return mi((ll) v * m2.inv().v); }
+    mi &operator+=(const mi &m2) { return *this = *this + m2; }
+    mi &operator-=(const mi &m2) { return *this = *this - m2; }
+    mi &operator*=(const mi &m2) { return *this = *this * m2; }
+    mi &operator/=(const mi &m2) { return *this = *this / m2; }
+    mi pow(ll e) const {
+        mi res = 1;
+        mi n = *this;
+        while (e > 0) {
+            if (e & 1) res *= n;
+            n *= n;
+            e >>= 1;
+        }
+        return res;
+    }
+    mi inv() const {
+        return pow(MOD - 2);
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    map<int, int> cnt;
+    rep(i, 0, n) {
+        int x;
+        cin >> x;
+        cnt[x]++;
+    }
+
+    mi ans = 1;
+    for (auto [_, c] : cnt) {
+        ans *= c + 1;
+    }
+
+    ans -= 1;
+    cout << ans.v << '\n';
+
+    return 0;
+}
