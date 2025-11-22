9ff1adc2e81391edcad79f3685af17cf3eaffeec
= <38575855+fishy15@users.noreply.github.com>
2025-11-21 23:55:05 -0500

    Sets of Complementary Sums (CF)

---
+++ online/cf/ed_181/sets_of_complementary_sums.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <numeric>
+
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define MOD 998244353
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+constexpr int MAXN = 637;
+
+struct mi {
+    int v;
+    mi() : mi(0) {}
+    mi(int _v) : v(_v) {
+        if (v >= MOD) v -= MOD;
+        if (v < 0) v += MOD;
+    }
+    mi(ll _v) : mi((int)(_v % MOD)) {}
+    mi operator+(const mi &m2) const { return mi(v + m2.v); }
+    mi operator-(const mi &m2) const { return mi(v - m2.v); }
+    mi operator*(const mi &m2) const { return mi((ll) v * m2.v); }
+    mi operator/(const mi &m2) const { return mi((ll) v * m2.inv().v); }
+    mi &operator+=(const mi &m2) { return *this = *this + m2; }
+    mi &operator-=(const mi &m2) { return *this = *this - m2; }
+    mi &operator*=(const mi &m2) { return *this = *this * m2; }
+    mi &operator/=(const mi &m2) { return *this = *this / m2; }
+    mi pow(ll e) const {
+        mi res = 1;
+        mi n = *this;
+        while (e > 0) {
+            if (e & 1) res *= n;
+            n *= n;
+            e >>= 1;
+        }
+        return res;
+    }
+    mi inv() const {
+        return pow(MOD - 2);
+    }
+};
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    if (n >= MAXN) {
+        cout << "0\n";
+        return;
+    }
+
+    // first thing will always take up 0 extra space 
+    vector<mi> dp(x + 1);
+    dp[0] = 1;
+    rep(i, 1, n) {
+        for (int j = i; j <= x; j++) {
+            dp[j] += dp[j - i];
+        }
+    }
+
+    vector<mi> psum = dp;
+    rep(i, 1, sz(psum)) {
+        psum[i] += psum[i - 1];
+    }
+
+    mi ans = 0;
+    rep(a1, 1, x+1) {
+        ll buffer = a1 + 1 - n;
+        buffer -= (ll) n * (n - 1) / 2;
+        if (buffer >= 0) {
+            ans += psum[min((ll) sz(psum) - 1, buffer)];
+        }
+    }
+    
+    cout << ans.v << '\n';
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
